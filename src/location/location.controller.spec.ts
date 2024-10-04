49655cad7a13c9954d98539e78759317bfa2f3d0
Roman Bylo <bylo.roman@gmail.com>
2024-10-04 12:41:47 +0300

    Fix location controller Unit test.

--- src/location/location.controller.spec.ts
+++ src/location/location.controller.spec.ts
@@ -1,29 +1,48 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { LocationController } from './location.controller';
 import { LocationService } from './location.service';
-
+import { MappingRegistryService } from '../common/mapping-registry.service';
+import { MockHelper } from '../common/mock.helper';
+import { GenericFactory } from '../common/generic.factory';
+import { Location } from './location.entity';
+import { LocationMappingService } from './location-mapping.service';
 
 describe('LocationController', () => {
   let locationController: LocationController;
-  let locationService: LocationService;
+  let mockLocationService: LocationService;
+  let mappingRegistryService: MappingRegistryService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [LocationController],
-      providers: [LocationService],
-    }).compile();
-
-    locationService = app.get<LocationService>(LocationService);
-    locationController = app.get<LocationController>(LocationController);
+    mockLocationService = MockHelper.mock<LocationService>({
+      list: {
+        resolves: [
+          GenericFactory.create<Location>(Location, {
+            id: '2e975edb-8a36-41e0-8a45-77fd47339e83',
+            name: 'Location 1',
+          }),
+          GenericFactory.create<Location>(Location, {
+            id: 'b05edbe4-2ffe-4ff7-91d8-bd80f07b968e',
+            name: 'Location 2',
+          }),
+        ],
+      },
+    });
+    mappingRegistryService = new MappingRegistryService();
+    const locationMappingService = new LocationMappingService(mappingRegistryService);
+    locationController = new LocationController(mockLocationService, mappingRegistryService);
   });
 
   describe('root', () => {
-    it('should return list of locations', () => {
-      expect(locationController.listLocations()).toMatchObject({
+    it('should return array of Locations', () => {
+      expect(locationController.listLocations()).resolves.toMatchObject({
         locations: [
-          'Location 1',
-          'Location 2',
-          'Location 3',
+          {
+            id: '2e975edb-8a36-41e0-8a45-77fd47339e83',
+            name: 'Location 1',
+          },
+          {
+            id: 'b05edbe4-2ffe-4ff7-91d8-bd80f07b968e',
+            name: 'Location 2',
+          },
         ],
       });
     });
